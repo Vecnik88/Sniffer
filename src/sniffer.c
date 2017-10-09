47ef5ce6bfb7d773224c425f6c075f6ddfce01ca
Vecnik <Ve_cni_k@inbox.ru>
2017-10-09 21:36:53 +0700

    src

--- src/sniffer.c
+++ src/sniffer.c
@@ -1,59 +1,7 @@
-#include <stdio.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <string.h>
-#include <arpa/inet.h>
-#include <netinet/ip.h>
-#include <netinet/udp.h>
-#include <netinet/tcp.h>
-#include <net/ethernet.h>
-#include <netinet/ip_icmp.h>
-#include <sys/socket.h>
-
-void package_processing(unsigned char* buffer, int size);
-void print_ip_header(unsigned char* buffer, int size);
-void print_tcp_packet(unsigned char* buffer, int size);
-void print_udp_packet(unsigned char * buffer, int size);
-void print_icmp_packet(unsigned char* buffer, int size);
-void print_data(unsigned char* buffer, int size);
+#include "../inc/sniffer.h"
 
 FILE* logfile = NULL;
 int tcp = 0, udp = 0, icmp = 0, others = 0, igmp = 0, total = 0;
-
-int main(int argc, char** argv)
-{
-	int raw_sock = 0;
-	int data_size = 0;
-	char* buffer = (char*) malloc(65536);
-	raw_sock = socket(PF_PACKET, SOCK_RAW, htons(ETH_P_ALL));
-
-	if (raw_sock == -1) {
-		perror("Error: create socket\n");
-		exit(EXIT_FAILURE);
-	}
-
-	logfile = fopen("log.txt", "w");
-	if (logfile == NULL)
-		printf("Unable to create file.\n");
-
-	printf("Starting...\n");
-
-	while (1) {
-		data_size = recvfrom(raw_sock, buffer, 65536, 0, NULL, NULL);
-
-		if (data_size < 0) {
-			printf("Recvfrom error, failed to get packets\n");
-			exit(EXIT_FAILURE);
-		}
-
-		package_processing(buffer + sizeof(struct ethhdr), data_size);
-	}
-
-	close(raw_sock);
-	printf("Finished\n");
-
-	return 0;
-}
 
 void package_processing(unsigned char* buffer, int size)
 {
